58e974cc4bccbe272e20497a53bb842bda021310
Scott Cho <scottsgcho@gmail.com>
2021-09-14 03:36:45 +0900

    clean

--- src/mappings/DefaultOS.ts
+++ src/mappings/DefaultOS.ts
@@ -4,27 +4,28 @@ import { Epoch as EpochTemplate } from '../../generated/templates';
 import { Members as MembersTemplate } from '../../generated/templates';
 import { Mining as MiningTemplate } from '../../generated/templates';
 import { PeerRewards as PeerRewardsTemplate } from '../../generated/templates';
-import { Token as TokenTemplate } from '../../generated/templates';
 import { Treasury as TreasuryTemplate } from '../../generated/templates';
+import { Token } from '../../generated/schema'
 
 export function handleModuleInstalled(event: ModuleInstalled): void {
-  switch(event.params.moduleKeycode) {
-    case(ByteArray.fromI32("EPC")):
+  switch(event.params.moduleKeycode.toHexString()) {
+    case("EPC"):
       EpochTemplate.create(event.params.module)
       break
-    case(ByteArray.fromI32("MBR")):
+    case("MBR"):
       MembersTemplate.create(event.params.module)
       break
-    case(ByteArray.fromI32("MNG")):
+    case("MNG"):
       MiningTemplate.create(event.params.module)
       break
-    case(ByteArray.fromI32("PAY")):
+    case("PAY"):
       PeerRewardsTemplate.create(event.params.module)
       break
-    case(ByteArray.fromI32("TKN")):
-      TokenTemplate.create(event.params.module)
+    case("TKN"):      
+      const token = new Token(event.params.module.toHexString())
+      token.save()
       break
-    case(ByteArray.fromI32("TSY")):
+    case("TSY"):
       TreasuryTemplate.create(event.params.module)
       break
   }  
