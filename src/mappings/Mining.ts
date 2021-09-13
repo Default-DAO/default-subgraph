a333ceaabfc5a19058523f7d5e83eb0d3fac314f
Conner Swenberg <cls364@cornell.edu>
2021-09-12 22:44:24 -0700

    Add handleMemberRegistered and handleRewardsClaimed mappings

--- src/mappings/Mining.ts
+++ src/mappings/Mining.ts
@@ -1,20 +1,48 @@
-import { 
+import { BigDecimal } from '@graphprotocol/graph-ts';
+
+import {
   RewardsIssued,
   RewardsClaimed,
-  MemberRegistered  
+  MemberRegistered
 } from '../../generated/templates/Mining/Mining';
 import {
   generateId,
 } from '../utils/helpers';
+import {
+  MiningRegistration,
+  Member,
+  EpochMemberInfo
+} from '../../generated/schema'
 
-export function handleRewardsIssued(event: RewardsIssued): void {  
+export function handleRewardsIssued(event: RewardsIssued): void {
+  const { os, issuer, currentEpoch, newRewardsPerShare, tokenBonus } = event.params
+
 
 }
 
-export function handleRewardsClaimed(event: RewardsClaimed): void {  
-  
+export function handleRewardsClaimed(event: RewardsClaimed): void {
+  const { os, epochClaimed, member, totalRewardsClaimed } = event.params
+
+  const reward = new BigDecimal(totalRewardsClaimed)
+
+  const memberSchema = Member.load(member.toHexString())
+  memberSchema.miningRewards = memberSchema.miningRewards.plus(reward)
+
+  const epochMemberSchema = EpochMemberInfo.load(generateId([os.toHexString(), epochClaimed, member.toHexString()]))
+  epochMemberSchema.miningRewards = epochMemberSchema.miningRewards.plus(reward)
+
+  memberSchema.save()
+  epochMemberSchema.save()
 }
 
-export function handleMemberRegistered(event: MemberRegistered): void {  
-  
+export function handleMemberRegistered(event: MemberRegistered): void {
+  const { os, currentEpoch, member } = event.params
+
+  const registration = new MiningRegistration(generateId([os.toHexString(), currentEpoch, member.toHexString()])) // should member be address or id?
+
+  registration.epoch = currentEpoch
+  registration.member = Member.load(member.toHexString()).id
+  registration.os = os.toHexString()
+
+  registration.save()
 }
